a368b579391b9fbcdb8712aec2ff1402e5a60962
Art Wild <wildart@gmail.com>
2014-05-29 17:41:39 -0400

    Recursive implementation of simple KD-Tree

--- src/kdtree.jl
+++ src/kdtree.jl
@@ -1,38 +1,39 @@
 import Base: setindex!, show
 
 using Base.Collections
+using Distance
 
 export KDTree, nearest
 
-abstract AbstractKDTree{K, V} <: AbstractNearestNeighborTree
+abstract AbstractKDTree{K} <: AbstractNearestNeighborTree
 
-type EmptyKDTree{K, V} <: AbstractKDTree{K, V}
+type EmptyKDTree{K} <: AbstractKDTree{K}
 end
 
-type KDTreeNode{K, V} <: AbstractKDTree{K, V}
+type KDTreeNode{K} <: AbstractKDTree{K}
     k::     K               # multidimensional point
-    v::		V
+    i::		Int             # index
     d::     Int             # split dimension
     s::     Float64         # split value
+    leaf::  Bool            # is leaf?
     left::  AbstractKDTree  # left node
     right:: AbstractKDTree  # right node
-    leaf::  Bool            # is leaf?
 
-    KDTreeNode(k::K, v::V) = new(k, v, 1, NaN, EmptyKDTree{K, V}(), EmptyKDTree{K, V}(), true)
-    KDTreeNode(k::K, v::V, d) = new(k, v, d, NaN, EmptyKDTree{K, V}(), EmptyKDTree{K, V}(), true)
+    KDTreeNode(k::K, i::Int) = new(k, i, 1, NaN, true, EmptyKDTree{K}(), EmptyKDTree{K}())
+    KDTreeNode(k::K, i::Int, d) = new(k, i, d, NaN, true, EmptyKDTree{K}(), EmptyKDTree{K}())
 end
 
-type KDTree{K, V} <: NearestNeighborTree
+type KDTree{K} <: NearestNeighborTree
 	metric::Metric
-	root:: AbstractKDTree{K, V}
+	root:: AbstractKDTree{K}
 
-	KDTree() = new (Euclidean(), EmptyKDTree{K, V}())
-	KDTree(metric::Metric) = new(metric, EmptyKDTree{K, V}())
-	KDTree(k, v, metric::Metric) = new(metric, setindex!(EmptyKDTree{K, V}(), v, k))
+	KDTree() = new (Euclidean(), EmptyKDTree{K}())
+	KDTree(metric::Metric) = new(metric, EmptyKDTree{K}())
+	KDTree(k, i, metric::Metric) = new(metric, setindex!(EmptyKDTree{K}(), i, k))
 
 	function KDTree(X::Matrix, metric::Metric)
 		n = size(X, 2)
-		t = KDTree{K, V}(X[:,1], 1, metric)
+		t = KDTree{K}(X[:,1], 1, metric)
 		for i = 2 : n
 			setindex!(t.root, i, X[:,i])
 		end
@@ -44,101 +45,91 @@ function show(io::IO, t::KDTree)
 	println(io, "KDTree: ", t.metric)
 end
 
-setindex!{K,V}(t::EmptyKDTree{K,V}, v, k) = KDTreeNode{K,V}(k, v)
-setindex!(t::KDTree, v, k) = (t.root = setindex!(t.root, v, k); t)
+setindex!{K}(t::EmptyKDTree{K}, i::Int, k) = KDTreeNode{K}(k, i)
+setindex!(t::KDTree, i::Int, k) = (t.root = setindex!(t.root, i, k); t)
 
-function setindex!{K,V}(n::KDTreeNode{K,V}, v::V, k::K)		
+function setindex!{K}(n::KDTreeNode{K}, i::Int, k::K)
 	if n.leaf
 		left_k = k
-		left_v = v
-		right_k = n.k		
-		right_v = n.v
+		left_i = i
+		right_k = n.k
+		right_i = n.i
 		if right_k[n.d] < left_k[n.d]
 			left_k = n.k
-			left_v = n.v
+			left_i = n.i
 			right_k = k
-			right_v = v		
+			right_i = i
 		end
 		n.s = 0.5*(right_k[n.d] + left_k[n.d])
 		next_sd = ((n.d) % length(k))+1
 
-		n.left = KDTreeNode{K,V}(left_k, left_v, next_sd)
-		n.right = KDTreeNode{K,V}(right_k, right_v, next_sd)
+		n.left = KDTreeNode{K}(left_k, left_i, next_sd)
+		n.right = KDTreeNode{K}(right_k, right_i, next_sd)
 		n.leaf = false
-	else 
-		setindex!(k[n.d] <= n.s ? n.left : n.right, v, k)
-	end 
+	else
+		setindex!(k[n.d] <= n.s ? n.left : n.right, i, k)
+	end
 	return n
 end
 
 disp(t::KDTree) = disp(t.root)
 function disp(n::KDTreeNode, l::Int = 0)
 	print(repeat("\t", l), "L: $(l), SD: $(n.d), SV: $(n.s)")
-	
+
 	if typeof(n.left) <: EmptyKDTree && typeof(n.right) <: EmptyKDTree
-		println(" = K: $(n.k), V: $(n.v)")
+		println(" = K: $(n.k) ($(n.i))")
 	else
 		println("")
 		disp(n.right, l+1)
-		disp(n.left, l+1)			
+		disp(n.left, l+1)
 	end
 end
 
-function nearest{K, V}(t::KDTree{K, V}, x::K, k::Int)	
-	stack = Array(KDTreeNode{K, V}, k)
-	h = Float64[]
-	index = Dict{Float64,V}()
-	sti = 1
-	stack[sti] = t.root
-	T = 1.0e10
-	while sti > 0 # length(stack) > 0 
-		n = stack[sti] # n = pop!(stack)		
-		sti -= 1
-		if n.leaf
-			d = evaluate(t.metric, x, n.k)			
-			if (length(h) < k) || (d < T)
-				while length(h) >= k
-					p = heappop!(h, Base.Order.Reverse) 				
-					pop!(index, p)
-				end
-				heappush!(h, d, Base.Order.Reverse)
-				push!(index, d, n.v)
-				T =  h[1]								
+function search_leaf{K}(n::KDTreeNode{K},
+	x::K,
+	k::Int,
+	h::Array{Float64,1},
+	index::Dict{Float64,Int},
+	m::Metric)
+
+	if n.leaf
+		d = evaluate(m, x, n.k)
+		if (length(h) < k) || (d < h[1])
+			while length(h) >= k
+				p = heappop!(h, Base.Order.Reverse)
+				pop!(index, p)
 			end
+			heappush!(h, d, Base.Order.Reverse)
+			push!(index, d, n.i)
+		end
+	else
+		# Determine nearest and furtherest branch
+		if (x[n.d] > n.s)
+			near = n.right
+			far = n.left
 		else
-			# Determine nearest and furtherest branch			
-			if (x[n.d] > n.s)
-				near = n.right
-				far = n.left
-			else
-				near = n.left
-				far = n.right
-			end
+			near = n.left
+			far = n.right
+		end
 
-			# Only search far tree if do not have enough neighbors	
-			d = (x[n.d]-n.s)*(x[n.d]-n.s)		
-			if length(h) < k || d <= T #evaluate(t.metric, x[n.d], n.s) <= T				
-				sti += 1
-				if sti > length(stack)
-					push!(stack, far)
-				else
-					stack[sti] = far
-				end	
-				# push!(stack, far)			
-			end
+		# Search the nearest branch
+		search_leaf(near, x, k, h, index, m)
 
-			# Search the nearest branch
-			sti += 1
-			if sti > length(stack)
-				push!(stack, near)
-			else
-				stack[sti] = near
-			end	
-			#push!(stack, near)		
+		# Only search far tree if do not have enough neighbors
+		d = evaluate(m, x[n.d], n.s)
+		if length(h) < k || d <= h[1]
+			search_leaf(far, x, k, h, index, m)
 		end
 	end
+end
 
-	vals = Array(eltype(index)[2], k)
+function nearest{K}(t::KDTree{K}, x::K, k::Int)
+	h = [Inf]
+	index = Dict{Float64,Int}(Inf,1)
+
+	search_leaf(t.root, x, k, h, index, t.metric)
+
+	vals = Array(Int, k)
 	dists = Array(Float64, k)
 	i = start(index)
 	j = 1
@@ -147,6 +138,9 @@ function nearest{K, V}(t::KDTree{K, V}, x::K, k::Int)
 		dists[j] = v[1]
 		vals[j] = v[2]
 		j += 1
-	end	
+	end
 	return vals, dists
 end
+
+# Distances redefiend
+Distance.evaluate{T1 <: FloatingPoint, T2 <: FloatingPoint}(dist::Euclidean, a::T1, b::T2) = (a-b)*(a-b)
--- test/kdtree.jl
+++ test/kdtree.jl
@@ -3,31 +3,33 @@ module TestKDTree
 	using Distance
 	using kNN
 	using StatsBase
-	
-	# Empty tree
-	t = KDTree{Vector,Int}()
-	@test typeof(t.root) == kNN.EmptyKDTree{Vector,Int}
 
-	t = KDTree{Vector,Int}(Euclidean())
+	# Empty tree
+	t = KDTree{Vector}()
+	@test typeof(t.root) == kNN.EmptyKDTree{Vector}
+
+	t = KDTree{Vector}(Euclidean())
 	@test typeof(t.root) <: kNN.EmptyKDTree
 	@test typeof(t.metric) == Euclidean
 
 	# With one element
 	x = [1, 2, 3]
-	t = KDTree{Vector{Int},Int}(x, 1, Euclidean())
-	@test typeof(t.root) == kNN.KDTreeNode{Vector{Int},Int}
+	t = KDTree{Vector{Int}}(x, 1, Euclidean())
+	@test typeof(t.root) <: kNN.KDTreeNode
 	@test t.root.k == x
-	@test t.root.v == 1	
+	@test t.root.i == 1
 
-	# With multiple points	
+	# With multiple points
 	X = [1  60  29  7 86  44 23 54 12]
-	t = KDTree{Vector{Int},Int}(X, Euclidean())
+	t = KDTree{Vector{Int}}(X, Euclidean())
 	@test typeof(t.root) <: kNN.KDTreeNode
 
 	# Search
+	Distance.evaluate(m::Metric, a::Int, b::Float64) = evaluate(m, float(a) ,b)
 	ind, dist = nearest(t, [3], 2)
 	@test length(ind) == 2
 	@test length(dist) ==  2
 	@test 4 in ind && 1 in ind
-	@test 4.0 in dist && 2.0 in dist
+	@test_approx_eq(4.0, dist[1])
+	@test_approx_eq(2.0, dist[2])
 end
